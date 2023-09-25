7393ce22fc9d9685dc6aa19261d1083333f8ad7a
shezedeth@gmail.com <sheze.gomez@gmail.com>
2023-09-25 20:27:53 -0300

    terminando tp_clase32

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,146 @@
+const db = require('../database/models');
+const sequelize = db.sequelize;
+const moment = require('moment');
+
+//Otra forma de llamar a los modelos
+const Movies = db.Movie;
+
+const moviesController = {
+    'list': (req, res) => {
+        db.Movie.findAll()
+            .then(movies => {
+                res.render('moviesList.ejs', {movies})
+            })
+    },
+    'detail': (req, res) => {
+        db.Movie.findByPk(req.params.id)
+            .then(movie => {
+                res.render('moviesDetail.ejs', {
+                    movie,
+                    Movie : movie,
+                    moment
+                });
+                
+            });
+            
+    },
+    'new': (req, res) => {
+        db.Movie.findAll({
+            order : [
+                ['release_date', 'DESC']
+            ],
+            limit: 5
+        })
+            .then(movies => {
+                res.render('newestMovies', {
+                    movies,
+                    Movie : movies,
+                    moment
+                });
+            });
+    },
+    'recomended': (req, res) => {
+        db.Movie.findAll({
+            where: {
+                rating: {[db.Sequelize.Op.gte] : 8}
+            },
+            order: [
+                ['rating', 'DESC']
+            ]
+        })
+            .then(movies => {
+                res.render('recommendedMovies.ejs', {movies});
+            });
+    }, //Aqui debemos modificar y completar lo necesario para trabajar con el CRUD
+    add: function (req, res) {
+        // TODO  
+        return res.render('moviesAdd');
+    },
+    create: function (req, res) {
+            const {title,rating,release_date,awards,length} = req.body;
+        db.Movie.create({
+            title : title.trim(),
+            rating,
+            awards,
+            release_date,
+            length
+        })
+            .then(movie => {
+            console.log(movie);
+                return res.redirect('/movies')
+            })
+    },
+    edit: function(req, res) {
+       // TODO
+       db.Movie.findByPk(req.params.id)
+            .then(movie => {
+            console.log(moment(movie.release_date).format('YYYY-MM-DD'))
+                return res.render('moviesEdit', {
+                    movie,
+                    Movie : movie,
+                    moment
+                })
+            })
+            .catch(error => console.log(error))
+      
+            
+    },
+    update: function (req,res) {
+        // TODO
+        const {title,rating,release_date,awards,length} = req.body;
+        
+        db.Movie.update(
+        {
+            title : title.trim(),
+            rating,
+            awards,
+            release_date,
+            length,
+            genre_id
+        },
+        {
+            where : {
+                id : req.params.id
+            }
+        }).then(response => {
+            console.log(response);
+            db.Movie.findByPk(req.params.id)
+                .then(movie => {
+                    return res.render('moviesDetail',{
+                        movie
+                    })
+                })
+            
+        }).catch(error => console.log(error))
+    },
+    delete: function (req, res) {
+        // TODO
+        db.Movie.findByPk(req.params.id)
+        .then(movie => {
+            return res.render('moviesDelete', {
+                movie,
+                Movie : movie
+                
+            })
+        })
+        .catch(error => console.log(error))
+  
+        
+    },
+    destroy: function (req, res) {
+        // TODO
+        db.Movie.destroy(
+            {
+               where: { id: req.params.id }
+            }
+        )
+        .then(() => {
+                   res.redirect('/movies');
+                })
+        
+          }
+    
+
+}
+
+module.exports = moviesController;
